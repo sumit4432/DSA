c1fcb0963665676c8a93cc58c0151587a15428c9
sumit4432 <spyadav30897@gmail.com>
2023-10-17 11:50:02 +0530

    insert data

--- insertElemnt/index.js
+++ insertElemnt/index.js
@@ -40,6 +40,8 @@
 // data[position]=newEle;
 // console.log(data);
 
+// // find max value
+
 // function findMax(arr){
 //     if(arr.length===0){
 //         return undefined
@@ -57,20 +59,83 @@
 // let res=findMax(myData)
 // console.log(res)
 
+// // remove dulicate
+// let myData=[1,2,3,45,55,55,7,77,77];
+// let unique=[];
+// for(let i=0; i<myData.length; i++){
+//     let element=myData[i];
+//     let duplicate=false;
+//     for(let j=0; j<unique.length; j++){
+//         if(element===unique[j]){
+//             duplicate=true;
+//             break;
+//         }
+//     }
+//     if(!duplicate){
+//         unique[unique.length]=element
+//     }
+// }
+// console.log(unique)
+// find second largest
 
-let myData=[1,2,3,45,55,55,7,77,77];
-let unique=[];
-for(let i=0; i<myData.length; i++){
-    let element=myData[i];
-    let duplicate=false;
-    for(let j=0; j<unique.length; j++){
-        if(element===unique[j]){
-            duplicate=true;
-            break;
-        }
-    }
-    if(!duplicate){
-        unique[unique.length]=element
-    }
-}
-console.log(unique)
+// let myData=[1,2,3,45,6775,4];
+// let first=-Infinity;
+// let second=-Infinity;
+// for(let i=0; i<myData.length; i++){
+//     if(myData[i] > first){
+//         second=first
+//         first=myData[i];
+//     }else if(myData[i] > second && myData[i] !== first){
+//         second=myData[i]
+//     }
+// }
+
+// console.log(second);
+// console.log(first)
+
+// //insert beging of array
+
+// let myData=[12,33,41,44,4];
+// let neItwm=23;
+// myData.unshift(neItwm);
+// console.log(myData)
+
+// insert end of the array
+
+// let data=[2,3,4,56,7];
+// let newData=232;
+// data.push(newData)
+// console.log(data)
+
+// insert specific  index value
+
+let data=[2,3,4,5,6,7];
+let element=23;
+data.splice(2,0,element);
+console.log(data)
+
+
+
+
+
+// //sum of zero first pairs
+
+
+
+
+
+
+// let data=[-5,-3,-2,-1, 0,2,3,4,8];
+// function paireZero(array){
+//     for(let number of array){
+//      for(let j=1; j<array.length; j++){
+//          if(number + array[j]===0){
+//              return [number, array[j]]
+//          }
+//      }
+//     }
+// }
+// let results= paireZero(data);
+// console.log(results);
+
+
